e4f952d12c7b1f0f644f1d4c3f4f9017a62ad008
Hasan <dihaz.nett@gmail.com>
2023-02-12 11:16:19 +0600

    ' '

--- src/Components/create-student.component.js
+++ src/Components/create-student.component.js
@@ -12,7 +12,7 @@ const CreateStudent = () => {
   // onSubmit handler
   const onSubmit = studentObject => {
     axios.post(
-'http://localhost:4000/students/create-student', 
+'http://localhost:4000/students/create-student',
     studentObject)
       .then(res => {
         if (res.status === 200){
